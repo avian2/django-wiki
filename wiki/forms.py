9c19a97f565476e46d1c0ebc166394543d5eeb91
Sebastián Ramírez Magrí <sebasmagri@gmail.com>
2011-02-22 03:18:50 -0430

    Some style refactoring

--- wiki/forms.py
+++ wiki/forms.py
@@ -2,7 +2,6 @@ from django import forms as forms
 
 from models import Page
 
-
 class PageForm(forms.Form):
     name = forms.CharField(max_length=255)
     content = forms.CharField(widget=forms.Textarea())
--- wiki/views.py
+++ wiki/views.py
@@ -5,20 +5,30 @@ from django.template import RequestContext
 from forms import PageForm
 from models import Page, Revision
 
-def index(request):
-    """Lists all pages stored in the wiki."""
+def index(request, template_name = 'wiki/index.html'):
+    '''
+    Lists all pages stored in the wiki
+    '''
     pages = Page.objects.all()
 
-    ctx = {'pages': pages}
-    return render_to_response('wiki/index.html', ctx, context_instance=RequestContext(request))
+    context = {
+        'pages' : pages
+        }
+    return render_to_response(
+        template_name,
+        context,
+        context_instance = RequestContext(request)
+        )
 
-def view_diff(request, name, rev=None):
-    """Shows the diffs for a wiki revision"""
+def view_diff(request, name, rev = None, template_name = 'wiki/diff.html'):
+    '''
+    Shows the diffs for a wiki revision
+    '''
     try:
-        page = Page.objects.get(name=name)
+        page = Page.objects.get(name = name)
         if rev is not None:
             rev = int(rev)
-            revision = get_object_or_404(Revision, page=page, counter=rev)
+            revision = get_object_or_404(Revision, page = page, counter = rev)
         else:
             revision = page.get_latest_revision()
     except Page.DoesNotExist:
@@ -31,35 +41,53 @@ def view_diff(request, name, rev=None):
     if revision and revision.get_prev():
         prev_content = revision.get_prev().content
 
-    #diff = difflib.HtmlDiff().make_table(revision.content.splitlines(), prev_content.splitlines())
     d = difflib.Differ()
     diff = d.compare(prev_content.splitlines(), revision.content.splitlines())
     diff = '\n'.join(list(diff))
 
-    ctx = { 'page': page, 'revision': revision, 'diff': diff }
-    return render_to_response('wiki/diff.html', ctx, context_instance=RequestContext(request))
+    context = {
+        'page' : page,
+        'revision' : revision,
+        'diff' : diff
+        }
+    return render_to_response(
+        template_name,
+        context,
+        context_instance=RequestContext(request)
+        )
 
-def view(request, name, rev=None):
-    """Shows a single wiki page."""
+def view(request, name, rev = None, template_name = 'wiki/view.html'):
+    '''
+    Shows a single wiki page
+    '''
     try:
-        page = Page.objects.get(name=name)
+        page = Page.objects.get(name = name)
         if rev is not None:
             rev = int(rev)
-            revision = get_object_or_404(Revision, page=page, counter=rev)
+            revision = get_object_or_404(Revision, page = page, counter = rev)
         else:
             revision = page.get_latest_revision()
     except Page.DoesNotExist:
         page = Page(name=name)
         revision = None
 
-    ctx = { 'page': page, 'revision': revision }
-    return render_to_response('wiki/view.html', ctx, context_instance=RequestContext(request))
+    context = {
+        'page' : page,
+        'revision' : revision
+        }
+    return render_to_response(
+        template_name,
+        context,
+        context_instance=RequestContext(request)
+        )
 
 
-def edit(request, name):
-    """Allows users to edit wiki pages."""
+def edit(request, name, template_name = 'wiki/edit.html'):
+    '''
+    Allows users to edit wiki pages
+    '''
     try:
-        page = Page.objects.get(name=name)
+        page = Page.objects.get(name = name)
     except Page.DoesNotExist:
         page = None
 
@@ -81,9 +109,24 @@ def edit(request, name):
     else:
         if page:
             revision = page.get_latest_revision()
-            form = PageForm(initial={'name': page.name, 'content': revision.content})
+            form = PageForm(
+                initial = {
+                    'name' : page.name,
+                    'content' : revision.content
+                    }
+                )
         else:
-            form = PageForm(initial={'name': name})
+            form = PageForm(
+                initial = {
+                    'name' : name,
+                    }
+                )
 
-    ctx = { 'form': form }
-    return render_to_response('wiki/edit.html', ctx, context_instance=RequestContext(request))
+    context = {
+        'form' : form,
+        }
+    return render_to_response(
+        template_name,
+        context,
+        context_instance=RequestContext(request)
+        )
